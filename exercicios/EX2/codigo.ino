a6c7bfc1bc6c41892638905092b5b618d78f0ef7
theodoramk <99494673+theodoramk@users.noreply.github.com>
2022-03-25 13:16:21 -0300

    Create codigo.ino

---
+++ exercicios/EX2/codigo.ino
@@ -0,0 +1,15 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT)
+}
+
+void loop()
+{
+  digitalWrite(2, HIGH);
+  delay(1000); )
+  digitalWrite(2, LOW);
+  delay(1000); //
+}
